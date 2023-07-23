5368ec557fbf2659e89e7404fb122d4154b114b9
Jay <jahanzad@JJs-MacBook-Pro.local>
2023-07-23 03:38:45 -0400

    removed old json file, updated new json with ALL exercises generated + custom metadata

--- SwiftJym/Models/Exercise.swift
+++ SwiftJym/Models/Exercise.swift
@@ -10,40 +10,56 @@ import SwiftUI
 
 // MARK: - Exercise
 struct Exercise: Hashable, Codable, Identifiable {
-    let id, name, force, level: String
-    let mechanic, equipment: String
+    let id: Int
+    let name, level: String
+    let force, mechanic, equipment: String
     let primaryMuscles, secondaryMuscles: [String]
     let instructions: [String]
     let category, imageName: String
+    let isFavorite, isFeatured, isRecent: Bool
 }
 
-//For testing and local development purposes
+
 #if DEBUG
 extension Exercise {
-    
-    static func trendingExample() -> [Exercise] {
-        [
-            Exercise(id: "1001", name: "Sumo Deadlift", force: "Pull", level: "Intermediate", mechanic: "Compound", equipment: "Barbell", primaryMuscles: ["Hamstrings"], secondaryMuscles: ["Lower Back", "Glutes"], instructions: ["Test Instructions for Bench Press"], category: "Weightlifting", imageName: "deadlift1"),
-            Exercise(id: "1002", name: "Crunches", force: "Core", level: "Beginner", mechanic: "Accessory", equipment: "Bodyweight", primaryMuscles: ["Abs"], secondaryMuscles: ["Lower Abs"], instructions: ["Test Instructions for Bench Press"], category: "Core", imageName: "crunches1"),
-            Exercise(id: "1003", name: "Bench Press", force: "Push", level: "Intermediate", mechanic: "Compound", equipment: "Barbell", primaryMuscles: ["Chest"], secondaryMuscles: ["Triceps"], instructions: ["Test Instructions for Bench Press"], category: "Weightlifting", imageName: "benchpress1")
-        ]
+
+    static func recentExercises() -> [Exercise] {
+        
+        var recentExercises = [Exercise]()
+        
+        for index in 0..<exercises.count {
+            if(exercises[index].isRecent){
+                recentExercises.append(exercises[index])
+            }
+        }
+        
+        return recentExercises
     }
     
-    static func recentExample() -> [Exercise] {
-        [
-            Exercise(id: "1001", name: "Sumo Deadlift", force: "Pull", level: "Intermediate", mechanic: "Compound", equipment: "Barbell", primaryMuscles: ["Hamstrings"], secondaryMuscles: ["Lower Back", "Glutes"], instructions: ["Test Instructions for Bench Press"], category: "Weightlifting", imageName: "deadlift1"),
-            Exercise(id: "1002", name: "Crunches", force: "Core", level: "Beginner", mechanic: "Accessory", equipment: "Bodyweight", primaryMuscles: ["Abs"], secondaryMuscles: ["Lower Abs"], instructions: ["Test Instructions for Bench Press"], category: "Core", imageName: "crunches1"),
-            Exercise(id: "1003", name: "Bench Press", force: "Push", level: "Intermediate", mechanic: "Compound", equipment: "Barbell", primaryMuscles: ["Chest"], secondaryMuscles: ["Triceps"], instructions: ["Test Instructions for Bench Press"], category: "Weightlifting", imageName: "benchpress1")
-        ]
+    
+    static func featuredExercises() -> [Exercise] {
+        
+        var featuredExercises = [Exercise]()
+        
+        for index in 0..<exercises.count {
+            if(exercises[index].isFeatured){
+                featuredExercises.append(exercises[index])
+            }
+        }
+        
+        return featuredExercises
     }
     
-    static func allExercisesExample() -> [Exercise] {
-        [
-            Exercise(id: "1001", name: "Sumo Deadlift", force: "Pull", level: "Intermediate", mechanic: "Compound", equipment: "Barbell", primaryMuscles: ["Hamstrings"], secondaryMuscles: ["Lower Back", "Glutes"], instructions: ["Test Instructions for Sumo Deadlift"], category: "Weightlifting", imageName: "deadlift1"),
-            Exercise(id: "1002", name: "Crunches", force: "Core", level: "Beginner", mechanic: "Accessory", equipment: "Bodyweight", primaryMuscles: ["Abs"], secondaryMuscles: ["Lower Abs"], instructions: ["Test Instructions for Crunches"], category: "Core", imageName: "crunches1"),
-            Exercise(id: "1003", name: "Bench Press", force: "Push", level: "Intermediate", mechanic: "Compound", equipment: "Barbell", primaryMuscles: ["Chest"], secondaryMuscles: ["Triceps"], instructions: ["Test Instructions for Bench Press"], category: "Weightlifting", imageName: "benchpress1")
-            
-        ]
+    
+    static func allExercises() -> [Exercise] {
+        
+        var allExercises = [Exercise]()
+        
+        for index in 0..<exercises.count {
+            allExercises.append(exercises[index])
+        }
+        
+        return allExercises
     }
 }
 #endif
